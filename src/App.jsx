1ceaa1d2ec86968be57b2916b506501363e51450
Razalp <razalp0012300@gmail.com>
2023-12-06 11:40:07 +0530

    fatches data from json place holder

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CoustumHookes from './componets/CoustumHookes'
 
 
 function App() {
+  const [data, setData] = useState([]);
   const {count , increment ,decrement}=CoustumHookes()
 const callback=()=>{
   console.log('hello am callback')
@@ -17,22 +18,46 @@ const callbackreturn=()=>{
   console.log('hello am unmounting callback')
 }
 
-  useEffect(()=>{
-    console.log('hello am mounting')
-    callback()
-    return()=>{
-      console.log('hello am unmounting')
-      callbackreturn()
+useEffect(() => {
+  console.log('hello am mounting');
+  callback();
+  return () => {
+    console.log('hello am unmounting');
+    callbackreturn();
+  };
+}, []);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.log(error);
     }
-  },[])
+  };
+  fetchData();
+}, []);
+
+  
 
   return (
     <>
+      <>
     <div>
       <h1>{count}</h1>
       <button onClick={decrement}>-</button>
       <button onClick={increment}>+</button>
+      <div>
+        {data.map((post) => (
+          <div key={post.id}>
+            <h1>{post?.id}</h1>
+          </div>
+        ))}
+      </div>
     </div>
+  </>
     
    
     </>
